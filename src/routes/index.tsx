46891c87ef9845ba2f3b5b816024403a2265ffb9
elpideus <elpideus@gmail.com>
2024-07-07 03:58:07 +0200

    Added some placeholders for the "Most Popular" section

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,33 @@
 import { component$ } from "@builder.io/qwik";
 export default component$(() => {
   return (
-      <main class="h-full w-full bg-default-bg rounded-lg">
+      <main class="h-full w-full bg-default-bg rounded-lg p-[10px]">
 
-        <div class="bg-[#2e2e2e] w-[200px] h-[200px]"></div>
+        <div class="w-full h-[300px] rounded-lg p-[10px] flex flex-col gap-[20px]">
+            <h2 class="text-[2rem] ml-[20px]">Most Popular</h2>
+            <div class="flex gap-[10px]">
+            <div class=" w-[160px] h-[200px] flex flex-col gap-[10px] p-[10px]">
+                <div class="bg-[violet] h-[90px]"></div>
+                <h3 class="text-[1.3rem]">Song Name</h3>
+                <h4 class="text-[1rem] opacity-80">Artist</h4>
+            </div>
+            <div class=" w-[160px] h-[200px] flex flex-col gap-[10px] p-[10px]">
+                <div class="bg-[violet] h-[90px]"></div>
+                <h3 class="text-[1.3rem]">Song Name</h3>
+                <h4 class="text-[1rem] opacity-80">Artist</h4>
+            </div>
+            <div class=" w-[160px] h-[200px] flex flex-col gap-[10px] p-[10px]">
+                <div class="bg-[violet] h-[90px]"></div>
+                <h3 class="text-[1.3rem]">Song Name</h3>
+                <h4 class="text-[1rem] opacity-80">Artist</h4>
+            </div>
+            <div class=" w-[160px] h-[200px] flex flex-col gap-[10px] p-[10px]">
+                <div class="bg-[violet] h-[90px]"></div>
+                <h3 class="text-[1.3rem]">Song Name</h3>
+                <h4 class="text-[1rem] opacity-80">Artist</h4>
+            </div>
+            </div>
+        </div>
 
       </main>
   );
